45e30c81630a28a4232b77f4327e49ae32aed623
angelabenavente <angela.b.almazan@gmail.com>
2019-11-14 19:01:22 +0100

    Add active share in twitter

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-console.log('>> Ready :)');
+const getElement = document.querySelector('#create-button');
+const shareCard =  document.querySelector("#share-card");
+
+function removeClass () {
+  shareCard.classList.remove('active-share');
+  //console.log('hola');
+}
+
+getElement.addEventListener('click',removeClass);
