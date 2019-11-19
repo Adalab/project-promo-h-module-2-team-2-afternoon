a59064833cf7e450e9dd92581aa628ab01b1083b
angelabenavente <angela.b.almazan@gmail.com>
2019-11-19 12:35:33 +0100

    Finish Arrow $ hidden JS

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,58 +1,68 @@
 'use strict';
 
-//Desplegar ventana de compartir card
+// TOOGLE PALETA COLORES 
+const arrowActive = document.querySelector('.arrow-button');
+const div =  document.querySelector("#js-toogle");
+const elementHidden = document.querySelector('#toogle');
 
-const getElement = document.querySelector('#create-button');
-const shareCard =  document.querySelector("#share-card");
+function toogle () {
+  if (elementHidden.classList.contains('hidden')) {
+    elementHidden.classList.remove('hidden');
+    arrowActive.classList.remove('fa-chevron-down');
+    arrowActive.classList.add('fa-chevron-up');
+  } else if (elementHidden.classList.contains('hidden')===false) {
+    elementHidden.classList.add('hidden'); 
+    arrowActive.classList.remove('fa-chevron-up');
+    arrowActive.classList.add('fa-chevron-down');
+  }
+}
+div.addEventListener('click', toogle);
 
-function removeClass () {
-  shareCard.classList.remove('active-share');
+//Desplegar formulario
+const arrowActiveForm = document.querySelector('.arrow-button-form');
+const form = document.querySelector('.fill__form')
+const headerForm = document.querySelector('.fill__header-container')
+const createFill = document.querySelector('.create__fill');
+
+function showNotshowForm () {
+  if (form.classList.contains('hidden')) {
+    form.classList.remove('hidden');
+    arrowActiveForm.classList.remove('fa-chevron-down');
+    arrowActiveForm.classList.add('fa-chevron-up');
+    createFill.style.padding = '0 0 30px 0';
+  } else if (form.classList.contains('hidden')===false) {
+    form.classList.add('hidden');
+    arrowActiveForm.classList.remove('fa-chevron-up');
+    arrowActiveForm.classList.add('fa-chevron-down');
+  }
+}
+headerForm.addEventListener('click', showNotshowForm);
+
+//Desplegar share card
+let createButton = document.querySelector('.create-button');
+let shareButton = document.querySelector('.active-share');
+let shareSection = document.querySelector('.create__share');
+const createIntroElement = document.querySelector('.create-intro');
+const arrowActiveShare = document.querySelector('.arrow-button-share');
+
+  function showNotShow() {
+    if (createButton.classList.contains('hidden')===true){
+      createButton.classList.remove('hidden');
+      arrowActiveShare.classList.remove('fa-chevron-down');
+      arrowActiveShare.classList.add('fa-chevron-up');
+    } else {
+      createButton.classList.add('hidden');
+      arrowActiveShare.classList.remove('fa-chevron-up');
+      arrowActiveShare.classList.add('fa-chevron-down');
+    }
+  }
+  createIntroElement.addEventListener('click',showNotShow);
+
+//Desplegar ventana de compartir card
+const getElement = document.querySelector('#create-button');
+const shareCard =  document.querySelector("#share-card");
+
+function removeClass () {
+  shareCard.classList.remove('active-share');
 }
 getElement.addEventListener('click',removeClass);
-
-//Cambiar flecha hacia abajo por flecha hacia arriba
-
-  let arrow = document.querySelector('.arrow-button');
-  let createButton = document.querySelector('.create-button');
-  let shareButton = document.querySelector('.active-share');
-  let shareSection = document.querySelector('.create__share');
-function changeArrow () {
-    
-  /* event.currentTarget.classList.toggle('teacher--selected');*/
-  event.currentTarget.classList.toggle('fa-chevron-up');
-    const arrowActive = event.currentTarget;
-    if (arrowActive.classList.contains('fa-chevron-down')) {
-      arrowActive.classList.remove('fa-chevron-down');
-      arrowActive.classList.add('fa-chevron-up');
-      createButton.style.display = 'none';
-      shareButton.style.display = 'none';
-      shareSection.style.padding = '0 0 7px 0';
-
-   /* } else if (arrowActive.classList.contains('fa-chevron-down') && createButton.style.display === none) {*/
-      
-  } else {
-    arrowActive.classList.remove('fa-chevron-up');
-    arrowActive.classList.add('fa-chevron-down');
-    createButton.style.display = 'block';
-      shareButton.style.display = 'default';
-      shareSection.style.padding = '0 0 30px 0';
-  }
-}
-arrow.addEventListener('click',changeArrow);
-
-
-
-
-
-// TOOGLE PALETA COLORES - falta
-const div =  document.querySelector("#js-toogle");
-const elementHidden = document.querySelector('#toogle');
-
-
-function toogle () {
-  if (elementHidden.classList.contains('hidden')) {
-    elementHidden.classList.remove('hidden'); 
-  }
-}
-div.addEventListener('click', toogle);
-
