b7b06606efbb7f47ef09fee49d0cd8feb83ed4f2
angelabenavente <angela.b.almazan@gmail.com>
2019-11-25 22:36:36 +0100

    Fix disabled button

--- _src/assets/js/04-share.js
+++ _src/assets/js/04-share.js
@@ -6,54 +6,20 @@ else {
 }*/
 const inputElements = document.querySelectorAll('input');
 const createButtonElement = document.getElementById('create-button');
-
+const error = document.querySelector('.error');
 function validateInputs() {
   for(let i = 0; i < inputElements.length; i++) {
     if (inputElements[i].value === '') {
       createButtonElement.disabled = true;
+      error.innerHTML = "Rellena todos los campos para continuar."
+      error.classList.remove('errorHidden');
+      error.classList.remove('hidden');
     } else {
       createButtonElement.disabled = false;
+      
     }
   }
 }
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-validateInputs();
